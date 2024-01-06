b0aea8c1603dad8677f602862da14820684d6a10
Shreya Singh <102823287+shreya-singh-parmar@users.noreply.github.com>
2024-01-06 11:41:28 +0530

    Add files via upload

---
+++ src/component/InputModal.jsx
@@ -0,0 +1,305 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Swal from "sweetalert2";
+import { v4 as uuidv4 } from 'uuid';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+};
+
+export default function InputModal(props) {
+    const { open, setOpen, data,
+        setData, recordId, firstName, setFirstName,
+        lastName, setLastName, email, setEmail,
+        phoneNumber, setPhoneNumber, updateFlag, setUpdateFlag } = props;
+
+    const [firstNameError, setFirstNameError] = React.useState(null);
+    const [lastNameError, setLastNameError] = React.useState(null);
+    const [emailError, setEmailError] = React.useState(null);
+    const [phoneNumberError, setPhoneNumberError] = React.useState(null);
+
+    function createData(firstName, lastName, email, phoneNumber) {
+        const id = uuidv4();
+        return { id, firstName, lastName, email, phoneNumber };
+    }
+    const setAllStateInitialValue = () => {
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPhoneNumber("");
+        setUpdateFlag("");
+    }
+
+    const setErrorInitialValue = () => {
+        setFirstNameError(false);
+        setLastNameError(false);
+        setEmailError(false);
+        setPhoneNumberError(false);
+    }
+    const handleClose = () => {
+        setOpen(false);
+        setAllStateInitialValue();
+        setErrorInitialValue();
+    }
+    const validateEmail = () => {
+        const regEmail = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
+        if (!email || !regEmail.test(email)) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    }
+    const validatePhoneNumber = () => {
+        setPhoneNumberError(true);
+        if (phoneNumber && phoneNumber.length === 9) {
+            setPhoneNumberError(false);
+        }
+    }
+
+    const handleAddRecord = () => {
+        const postData = createData(firstName, lastName, email, phoneNumber);
+        if (data.length === 0) {
+            setData([postData]);
+            localStorage.setItem("empData", JSON.stringify([postData]));
+        } else {
+            setData([...data, postData]);
+            localStorage.setItem("empData", JSON.stringify([...data, postData]));
+        }
+        Swal.fire(
+            'Record has been added successfully !!',
+            '',
+            'success'
+        )
+        setOpen(false);
+        setAllStateInitialValue();
+        setErrorInitialValue();
+    }
+
+    const handleUpdateRecord = () => {
+        const myData = data.map(x => {
+            if (x.id === recordId) {
+                return {
+                    ...x,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    phoneNumber: phoneNumber,
+                    id: recordId
+                }
+            }
+            return x;
+        })
+        Swal.fire({
+            title: 'Do you want to update the details?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Yes Update',
+            denyButtonText: `Don't Update`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setData(myData);
+                localStorage.setItem("empData", JSON.stringify(myData));
+                Swal.fire('Record has been Updated successfully !!', '', 'success')
+            } else if (result.isDenied) {
+                Swal.fire('Changes are not saved', '', 'info')
+            }
+        })
+        setOpen(false);
+        setAllStateInitialValue();
+        setErrorInitialValue();
+    }
+
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Card sx={{ width: '100%', padding: 2 }}>
+                            <CardHeader
+                                title={updateFlag ? "Update Employee Details " : "Add New Employee Details"}
+                                subheader={new Date().toString().slice(0, 15)}
+                            />
+                            <CardContent>
+                                <TextField
+                                    required
+                                    margin="normal"
+                                    label="First Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={firstName}
+                                    onChange={(e) => {
+                                        setFirstName(e.target.value);
+                                    }}
+                                />
+                                {firstNameError && <p style={{ color: 'red' }}>This Field is Required!</p>}
+                                <TextField
+                                    required
+                                    margin="normal"
+                                    label="Last Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={lastName}
+                                    onChange={(e) => {
+                                        setLastName(e.target.value);
+                                    }}
+                                />
+                                {lastNameError && <p style={{ color: 'red' }}>This Field is Required!</p>}
+                                <TextField
+                                    required
+                                    margin="normal"
+                                    label="Email"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={email}
+                                    onChange={(e) => {
+                                        setEmail(e.target.value)
+                                        validateEmail();
+                                    }}
+                                />
+                                {emailError && <p style={{ color: 'red' }}>Enter a Valid Email!</p>}
+                                <TextField
+                                    required
+                                    margin="normal"
+                                    label="Phone Number"
+                                    variant="outlined"
+                                    fullWidth
+                                    value={phoneNumber}
+                                    onChange={(e) => {
+                                        setPhoneNumber(e.target.value);
+                                        validatePhoneNumber();
+                                    }}
+                                />
+                                {phoneNumberError && <p style={{ color: 'red' }}>Enter a valid Phone number(10 digits only)!</p>}
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-between"
+                                    alignItems="center"
+                                    spacing={2}
+                                    sx={{ width: '100%' }}
+                                >
+                                    <Button
+                                        variant="contained"
+                                        color="error"
+                                        sx={{
+                                            fontWeight: '600',
+                                            borderRadius: '10px',
+                                            height: "50px",
+                                            width: "100px",
+                                        }}
+                                        onClick={handleClose}
+                                    >
+                                        <Stack
+                                            direction="row"
+                                            justifyContent="center"
+                                            alignItems="center"
+                                            spacing={2}
+                                            sx={{ width: '100%' }}
+                                        >
+                                            <CancelIcon sx={{ marginRight: '10px' }} />
+                                            Close
+                                        </Stack>
+                                    </Button>
+                                    {
+                                        updateFlag && (
+                                            <Button
+                                                disabled={
+                                                    (firstName &&
+                                                        lastName &&
+                                                        phoneNumber &&
+                                                        !phoneNumberError &&
+                                                        email &&
+                                                        !emailError) ? false : true
+                                                }
+                                                variant="contained"
+                                                color="success"
+                                                sx={{
+                                                    fontWeight: '600',
+                                                    borderRadius: '10px',
+                                                    height: "50px",
+                                                    width: "100px"
+                                                }}
+                                                onClick={handleUpdateRecord}
+                                            >
+                                                <Stack
+                                                    direction="row"
+                                                    justifyContent="center"
+                                                    alignItems="center"
+                                                    spacing={2}
+                                                    sx={{ width: '100%' }}
+                                                >
+                                                    <AddTaskIcon sx={{ marginRight: '10px' }} />
+                                                    Update
+                                                </Stack>
+                                            </Button>
+                                        )
+                                    }
+                                    {
+                                        !updateFlag && (
+                                            <Button
+                                                disabled={
+                                                    (firstName &&
+                                                        lastName &&
+                                                        phoneNumber &&
+                                                        !phoneNumberError &&
+                                                        email &&
+                                                        !emailError) ? false : true
+                                                }
+                                                variant="contained"
+                                                color="success"
+                                                sx={{
+                                                    fontWeight: '600',
+                                                    borderRadius: '10px',
+                                                    height: "50px",
+                                                    width: "100px"
+                                                }}
+                                                onClick={handleAddRecord}
+                                            >
+                                                <Stack
+                                                    direction="row"
+                                                    justifyContent="center"
+                                                    alignItems="center"
+                                                    spacing={2}
+                                                    sx={{ width: '100%' }}
+                                                >
+                                                    <AddTaskIcon sx={{ marginRight: '10px' }} />
+                                                    Add
+                                                </Stack>
+                                            </Button>
+                                        )
+                                    }
+                                </Stack>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div >
+    );
+}
---
+++ src/page/Home.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import DisplayTable from "../component/DisplayTable";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import InputModal from "../component/InputModal";
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
+import Swal from "sweetalert2";
+
+const Home = () => {
+  const [data, setData] = React.useState([]);
+  const [openModal, setOpenModal] = React.useState(false);
+  const [recordId, setRecordId] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [updateFlag, setUpdateFlag] = React.useState(false);
+
+  const handleUpdate = (id) => {
+    const recordData = data.filter((val) => val.id === id);
+    setUpdateFlag(true);
+    setRecordId(recordData[0].id);
+    setFirstName(recordData[0].firstName);
+    setLastName(recordData[0].lastName);
+    setEmail(recordData[0].email);
+    setPhoneNumber(recordData[0].phoneNumber);
+    setOpenModal(true);
+  }
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const deleteData = data.filter((val) => val.id !== id);
+        setData(deleteData);
+        localStorage.setItem("empData", JSON.stringify(deleteData));
+        Swal.fire(
+          'Deleted!',
+          'Record has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    const getData = JSON.parse(localStorage.getItem("empData"));
+    if (getData == null) {
+      setData([])
+    } else {
+      setData(getData);
+    }
+  }, [])
+
+  return (
+    <Container sx={{ marginTop: '100px', marginBottom: '50px' }}>
+      <Box sx={{ textAlign: 'center', marginBottom: '50px' }}>
+        <Typography variant="h4" sx={{ fontWeight: '800', textTransform: 'uppercase' }}>
+          Employee Database
+        </Typography>
+      </Box>
+      <Button
+        variant="contained"
+        color="success"
+        sx={{
+          fontWeight: '600',
+          borderRadius: '10px',
+          height: "50px",
+          width: "150px",
+        }}
+        onClick={() => {
+          setOpenModal(!openModal)
+        }}
+      >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+          sx={{ width: '100%' }}
+        >
+          <AddIcon sx={{ marginRight: '10px' }} />
+          Add new Record
+        </Stack>
+      </Button>
+      {
+        data.length === 0 ? (
+          <>
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+              sx={{
+                width: '100%',
+                marginTop: '20px'
+              }}
+            >
+              <img src="./NoData.png" alt="No Data Found!" />
+              <Typography variant="h6" sx={{ fontWeight: '800' }}>
+                No Record Found!
+              </Typography>
+              <Typography variant="button1" sx={{ fontWeight: '600' }}>
+                Click On "Add new Record" Button.
+              </Typography>
+            </Stack>
+          </>
+        ) : (
+          <>
+            <DisplayTable
+              rows={data}
+              handleUpdate={handleUpdate}
+              handleDelete={handleDelete}
+            />
+          </>
+        )
+      }
+      <InputModal
+        data={data}
+        recordId={recordId}
+        firstName={firstName}
+        setFirstName={setFirstName}
+        lastName={lastName}
+        setLastName={setLastName}
+        email={email}
+        setEmail={setEmail}
+        phoneNumber={phoneNumber}
+        setPhoneNumber={setPhoneNumber}
+        setData={setData}
+        updateFlag={updateFlag}
+        setUpdateFlag={setUpdateFlag}
+        open={openModal}
+        setOpen={setOpenModal}
+      />
+    </Container>
+  );
+};
+
+export default Home;
